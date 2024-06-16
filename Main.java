ca794cdddeaf8281830318e9b3552f2f8f1e6292
nemanja <95zakon@gmail.com>
2024-06-16 21:42:45 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			StackPane root = new StackPane();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainFx.fxml"));
+			Pane pane = loader.load();
+			root.getChildren().add(pane);
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
